c332e3b3aec7fbc2c588616de860640d5460ceec
Miguel CR <camposmiguel@gmail.com>
2018-04-17 19:36:21 +0200

    Calc v1

---
+++ 01_Layouts/02_Calc/app/src/main/java/com/miguelcamposrivera/a02_calc/MainActivity.java
@@ -0,0 +1,43 @@
+package com.miguelcamposrivera.a02_calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // IU Components
+    TextView screen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Connect the 'screen' variable with the textViewScreen component
+        screen = findViewById(R.id.textViewScreen);
+
+        // Set a new value to the textViewScreen
+        screen.setText("0");
+    }
+
+    public void numberClicked(View view) {
+        // Button information
+        Button buttonClicked = (Button)view;
+        String buttonNumber = buttonClicked.getText().toString();
+        String currentScreenText = screen.getText().toString();
+
+        if(currentScreenText.equals("0")) {
+            // If is the first time, we don't need to concatenate
+            screen.setText(buttonNumber);
+        } else {
+            screen.setText(currentScreenText + buttonNumber);
+        }
+    }
+
+    public void deleteClicked(View view) {
+        // Write the code to delete
+        // the last char in the screen
+    }
+}
