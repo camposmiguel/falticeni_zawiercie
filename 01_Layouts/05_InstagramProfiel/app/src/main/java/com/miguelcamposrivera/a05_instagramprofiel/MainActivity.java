1b79134ecb5649c37ea4a8a8595e9549deee4fc6
Miguel CR <camposmiguel@gmail.com>
2018-04-19 14:29:03 +0200

    layouts

--- 01_Layouts/05_InstagramProfiel/app/src/main/java/com/miguelcamposrivera/a05_instagramprofiel/MainActivity.java
+++ 01_Layouts/05_InstagramProfiel/app/src/main/java/com/miguelcamposrivera/a05_instagramprofiel/MainActivity.java
@@ -5,13 +5,21 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileImage = findViewById(R.id.imageViewProfile);
+
+        Picasso.get().load("https://instagram.fmad8-1.fna.fbcdn.net/vp/c6b4dbf4752e03afb97453c4782955f3/5B51ACF0/t51.2885-19/s320x320/27580324_1961241000859897_4541351977585475584_n.jpg").into(profileImage);
     }
 
     public void openWebsite(View view) {
