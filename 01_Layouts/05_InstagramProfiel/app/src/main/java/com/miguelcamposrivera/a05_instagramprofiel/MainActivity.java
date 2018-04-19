edc2648eae04a69cb8ca6e8f0c5f0d3c0ee1b626
Miguel CR <camposmiguel@gmail.com>
2018-04-19 13:27:31 +0200

    InstagramProfile v2

--- 01_Layouts/05_InstagramProfiel/app/src/main/java/com/miguelcamposrivera/a05_instagramprofiel/MainActivity.java
+++ 01_Layouts/05_InstagramProfiel/app/src/main/java/com/miguelcamposrivera/a05_instagramprofiel/MainActivity.java
@@ -1,7 +1,10 @@
 package com.miguelcamposrivera.a05_instagramprofiel;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+        // The website address we want to open
+        Uri webpage = Uri.parse("http://depechemode.com");
+
+        // We're defining an Intent object to call to external
+        // application.
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void callPhone(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:954112233"));
+        startActivity(intent);
     }
 }
