307e7bd3093c5b2b8430fbf6cfbecbf41d3e6122
Miguel CR <camposmiguel@gmail.com>
2018-04-20 21:46:14 +0200

    v3

--- 02_Navigation/01_Intent_explicits/app/src/main/java/com/miguelcamposrivera/a01_intent_explicits/MainActivity.java
+++ 02_Navigation/01_Intent_explicits/app/src/main/java/com/miguelcamposrivera/a01_intent_explicits/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         String surname = extras.getString("last_name");
         String sex = extras.getString("sex");
         int number = extras.getInt("random_number");
-    
+
 
         // Set the information into the TextView components
         tvName.setText(name);
