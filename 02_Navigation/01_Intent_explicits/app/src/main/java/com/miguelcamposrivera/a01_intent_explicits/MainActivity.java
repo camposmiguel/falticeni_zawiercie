e5648b0750c953d3a276b616ae170b27eac2fdd9
Miguel CR <camposmiguel@gmail.com>
2018-04-20 12:16:08 +0200

    signup app
    Complete exercise

--- 02_Navigation/01_Intent_explicits/app/src/main/java/com/miguelcamposrivera/a01_intent_explicits/MainActivity.java
+++ 02_Navigation/01_Intent_explicits/app/src/main/java/com/miguelcamposrivera/a01_intent_explicits/MainActivity.java
@@ -2,12 +2,32 @@ package com.miguelcamposrivera.a01_intent_explicits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvName, tvSurname, tvSex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a refence for each view component
+        tvName = findViewById(R.id.textViewName);
+        tvSurname = findViewById(R.id.textViewSurname);
+        tvSex = findViewById(R.id.textViewSex);
+
+        // Get the information that we receive from the SignUpActivity
+        Bundle extras = getIntent().getExtras();
+
+        String name = extras.getString("first_name");
+        String surname = extras.getString("last_name");
+        String sex = extras.getString("sex");
+
+        // Set the information into the TextView components
+        tvName.setText(name);
+        tvSurname.setText(surname);
+        tvSex.setText(sex);
+
     }
 }
